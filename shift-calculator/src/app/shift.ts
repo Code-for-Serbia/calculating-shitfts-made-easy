6299e178467787215497a4b214f6264754117822
itmilos <milosrujevic@gmail.com>
2023-05-03 23:41:51 +0200

    github pages

--- shift-calculator/src/app/shift.ts
+++
@@ -1,9 +0,0 @@
-export class Shift {
-  name: string = '';
-  year?: number;
-  startHour: string = '';
-  endHour: string = '';
-  hourlyRate: number = 1;
-  duration: number = 1;
-  salaryByDay: number = 1;
-}
